244aedd6630c33303e70c9281965f0ee9a703249
yuranb <yuhaoz0120@gmail.com>
2024-02-28 22:42:46 -0700

    Fetch Weather Data from API

--- script.js
+++ script.js
@@ -24,3 +24,13 @@ weatherSearchForm.addEventListener('submit', function(e) {
     mockDatabase.queryHistory.push(cityName);
     updateQueryHistoryDisplay();
 });
+
+// Function to fetch weather data using OpenWeatherMap API
+function fetchWeather(city) {
+    const apiKey = "13e786b2255800d48474a6fdbcd071dc";
+    fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    )
+    .then((response) => response.json())
+    .then((data) => displayWeather(data));
+}
