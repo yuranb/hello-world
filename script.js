6bd4a7f3738326d86401aba2b29f019a7dc46d35
yuranb <yuhaoz0120@gmail.com>
2024-02-28 22:23:52 -0700

    Handle Document Ready for Initialization

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-// Define a mock database to store query history
+document.addEventListener('DOMContentLoaded', function() {
+    const weatherSearchForm = document.querySelector('.weather-search form');
+    const loginForm = document.querySelector('.login-form');
+    const queryHistoryList = document.querySelector('.database-data-placeholder ul');
+    const realtimeTempElement = document.getElementById('realtime-temp');
+    // Define a mock database to store query history
 let mockDatabase = {
     queryHistory: [],
   };
