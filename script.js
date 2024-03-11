302514b079d4fdaca92bfc759abd5af968f76d33
yuranb <yuhaoz0120@gmail.com>
2024-03-10 22:51:51 -0600

    Added a function to listen to the search key and fix a bug in calling the api.

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const weather = {
-        apiKey = "ea9f40b3e63d13331a1f878412420312",
-    }
-// Function to fetch weather data using OpenWeatherMap API
-function fetchWeather(city) {
-        fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
-        )
-        .then((response) => response.json())
-        .then((data) => displayWeather(data))
-        .catch(error => console.error("Error fetching weather data:", error));
-    }
+        apiKey : "ea9f40b3e63d13331a1f878412420312",
+    };
+    
+    // Function to fetch weather data using OpenWeatherMap API
+    function fetchWeather(city) {
+            fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weather.apiKey}&units=imperial`
+            )
+            .then((response) => response.json())
+            .then((data) => displayWeather(data))
+            .catch(error => console.error("Error fetching weather data:", error));
+        }
 
 // Function to update query history display on the webpage
 /*function updateQueryHistoryDisplay() {
@@ -22,20 +23,28 @@ function fetchWeather(city) {
     });
 }*/
 
+    // Display fetched weather data on the webpage
+    function displayWeather(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city-name").innerText = name;
+        document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}.png`;
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temperature").innerText = `${temp}` + "°F";
+        document.querySelector(".humidity").innerText = ` ${humidity}` + "%";
+        document.querySelector(".wind-speed").innerText = ` ${speed} ` + "m/s";
+    }
 
-
-
-// Display fetched weather data on the webpage
-
-function displayWeather(data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    document.querySelector(".city-name").innerText = name;
-    document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}.png`;
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temperature").innerText = `${temp}` + "°F";
-    document.querySelector(".humidity").innerText = ` ${humidity}` + "%";
-    document.querySelector(".wind-speed").innerText = ` ${speed} ` + "m/s";
-}
+    const searchButton = document.querySelector(".weather-search button");
+    searchButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        const searchBar = document.querySelector(".search-bar");
+        const city = searchBar.value;
+        if (city) {
+            fetchWeather(city);
+            searchBar.value = ""; 
+        }
+    });
+});
