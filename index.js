0041c8b8552f25a5b8baaa2b7ef3422a217c0b9d
yuranb <yuhaoz0120@gmail.com>
2024-04-11 15:41:04 -0600

    Importing node-fetch with the es module

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const fetch = require('node-fetch'); 
+import express from 'express';
+import fetch from 'node-fetch'; 
 const app = express();
 
 app.use(express.static('public'))
