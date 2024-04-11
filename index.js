01309730655819307d3bb88b17daf42407e87179
yuranb <yuhaoz0120@gmail.com>
2024-04-11 00:52:00 -0600

    isntall node.fetch and add Middleware

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const fetch = require('node-fetch'); 
 const app = express();
 
-app.use(express.static('Public'))
+app.use(express.static('public'))
+
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 const port = 4000;
+
+const apiRouter = express.Router();
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
