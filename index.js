ae5d606f7948047202bf534b503f14e084180e44
yuranb <yuhaoz0120@gmail.com>
2024-04-11 01:21:18 -0600

    Add an endpoint to call the OpenWeatherMap API

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const port = 4000;
 
 const apiRouter = express.Router();
 
+apiRouter.get('/weather/:city', async (req, res) => {
+    const city = req.params.city;
+    const apiKey = 'ea9f40b3e63d13331a1f878412420312';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    try {
+
+});
+
+app.use('/api', apiRouter);
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
