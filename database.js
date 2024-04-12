a7e77bb6e7af0084e4644c69a25aee9e4257b5e0
yuranb <yuhaoz0120@gmail.com>
2024-04-12 04:01:43 -0600

    Updated database to add the userCollection

--- database.js
+++ database.js
@@ -6,7 +6,9 @@ const uuid = require('uuid');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db =  client.db('SkyWatchDB');
+const db =  client.db('WeatherDB');
+const userCollection = db.collection('users');
+const queryHistoryCollection = db.collection('queryHistory');
 
 (async function testConnection() {
     await client.connect();
@@ -15,3 +17,32 @@ const db =  client.db('SkyWatchDB');
     console.log(`Unable to connect to database with ${url} because ${ex.message}`);
     process.exit(1);
   });
+
+async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+    const newUser = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    await userCollection.insertOne(newUser);
+    return newUser;
+    }
+
+function findUser(email) {
+    return userCollection.findOne({ email });
+}
+
+async function validateUser(email, password) {
+    const user = await findUser(email);
+    if (user && await bcrypt.compare(password, user.password)) {
+        return user;
+    }
+    return null; 
+}
+
+module.exports = {
+    createUser,
+    findUser,
+    validateUser
+};
