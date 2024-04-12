7364b40d8561284b9d09efac10c278f978587fe4
yuranb <yuhaoz0120@gmail.com>
2024-04-12 02:28:38 -0600

    installed bcrypt,uuid and updated basic database

---
+++ database.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db =  client.db('SkyWatchDB');
+
+(async function testConnection() {
+    await client.connect();
+    await db.command({ ping: 1 });
+  })().catch((ex) => {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  });
