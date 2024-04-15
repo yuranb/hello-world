c0fec71f2d1966e741203544f629a085bf7103f1
yuranb <yuhaoz0120@gmail.com>
2024-04-15 13:34:22 -0600

    Updated peerProxy.js

--- peerProxy.js
+++ peerProxy.js
@@ -1,34 +1,43 @@
 const { WebSocketServer } = require('ws');
 const uuid = require('uuid');
+const axios  = require('axios')
 
+const getWeather = async (ws) => {
+  const apiKey = 'ea9f40b3e63d13331a1f878412420312';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Provo&appid=${apiKey}&units=imperial`;
+  try {
+      const response = await axios.get(url);
+      const data = response.data
+      if (response.status===200) {
+        ws.send(JSON.stringify(data))
+      }
+  } catch (error) {
+      console.error('Error fetching weather data:', error);
+  }
+}
 function peerProxy(httpServer) {
-  // Create a websocket object
   const wss = new WebSocketServer({ noServer: true });
-
-  // Handle the protocol upgrade from HTTP to WebSocket
   httpServer.on('upgrade', (request, socket, head) => {
     wss.handleUpgrade(request, socket, head, function done(ws) {
       wss.emit('connection', ws, request);
     });
   });
-
-  // Keep track of all the connections
   let connections = [];
 
   wss.on('connection', (ws) => {
+    setInterval(function send() {
+      getWeather(ws)
+    }, 3600000);
     const connection = { id: uuid.v4(), alive: true, ws: ws };
     connections.push(connection);
-
-    // Forward messages to everyone except the sender
     ws.on('message', function message(data) {
+     
       connections.forEach((c) => {
         if (c.id !== connection.id) {
           c.ws.send(data);
         }
       });
     });
-
-    // Remove the closed connection so we don't try to forward anymore
     ws.on('close', () => {
       const pos = connections.findIndex((o, i) => o.id === connection.id);
 
@@ -36,17 +45,12 @@ function peerProxy(httpServer) {
         connections.splice(pos, 1);
       }
     });
-
-    // Respond to pong messages by marking the connection alive
     ws.on('pong', () => {
       connection.alive = true;
     });
   });
-
-  // Keep active connections alive
   setInterval(() => {
     connections.forEach((c) => {
-      // Kill any connection that didn't respond to the ping last time
       if (!c.alive) {
         c.ws.terminate();
       } else {
