1df4d13d241d73d311b3bdb17c06295aff0a6bdb
yuranb <yuhaoz0120@gmail.com>
2024-04-16 23:24:24 -0600

    Updated about/index.jsx for the about page

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,18 @@
+const About = ()=>{
+    return (
+      <div>
+            <main>
+          <section className="about-content">
+            <h2>Weather Query Features:</h2>
+            <p>Weather Query uses OpenWeather's API to provide you with accurate and up-to-date weather information to help you plan your day efficiently.</p>
+            <h3>Provo's Weather</h3>
+            <p>Displays Provo's weather by default and updates it regularly on an hourly basis.</p>
+            <h3>User Search History</h3>
+            <p>After logging in, you can see the search results of other users.</p>
+          </section>
+        </main>
+      </div>
+    )
+  }
+  
+  export default About;
