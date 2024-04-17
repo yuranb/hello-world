630cbe6a2b899775ced038ddca39b207fff8ca7b
yuranb <yuhaoz0120@gmail.com>
2024-04-16 20:54:15 -0600

    Updated login/index.jsx for react login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,8 +1,52 @@
 import { useNavigate } from "react-router-dom";
 const Login = ()=>{
   const navigate = useNavigate()
+
+  async function loginUser() {
+    const email = document.querySelector('#userName').value;
+    const password = document.querySelector('#userPassword').value;
+  
+    const response = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+  
+    if (response.ok) {
+      localStorage.setItem('userName', email);
+      alert('Log in successfully!');
+     navigate('/history')
+    } else {
+      alert('Login failed, please check your email and password.');
+    }
+  }
   return (
-    <div className="login">login</div>
+    <div className="login">
+             <div id="loginControls">
+          <div className="my-3">
+            <input
+              type="text"
+              className="form-control"
+              id="userName"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="userPassword"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button className="btn btn-primary mr-3" onClick={()=>{loginUser()}}>Login</button>
+          <button className="btn btn-secondary" onClick={navigate('/registry')}>
+            Register
+          </button>
+        </div>
+    </div>
   )
 }
 export default Login;
